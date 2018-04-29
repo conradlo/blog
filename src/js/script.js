50e8549556d09b1b2cfbead4eea3d1f7887ab534
Conrad <email@conradlo.com>
2018-04-29 14:30:34 +0800

    write README in org format

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,38 @@
-window.onload = function () {
-  setTimeout(function () {
-    window.scrollTo(0, 1);
-  }, 100);
-};
+// window.onload = function () {
+//   setTimeout(function () {
+//     window.scrollTo(0, 1);
+//   }, 100);
+// };
 
 // disable touch scrolling on mobile
-document.addEventListener("touchmove", function (event) {
-  event.preventDefault();
-}, false);
+// document.addEventListener("touchmove", function (event) {
+//   event.preventDefault();
+// }, false);
 
 (function() {
-  // console.log('ready');
+  console.log('ready');
   // console.log(navigator.userAgent);
 })();
+
+// Scroll Magic
+// http://scrollmagic.io/docs/index.html
+
+$(function() {
+    // init
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: 'onLeave'
+        }
+    });
+
+    var slides = document.querySelectorAll("section.panel");
+
+    for (var i = 0; i < slides.length; i++) {
+        new ScrollMagic.Scene({
+            triggerElement: slides[i]
+        })
+            .setPin(slides[i])
+            // .addIndicators()
+            .addTo(controller);
+    }
+});
