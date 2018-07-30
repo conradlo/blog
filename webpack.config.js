72b09afbf269d40800b31599dadf6d4397343cdc
Conrad Lo <email@conradlo.com>
2018-07-30 23:59:11 +0800

    [wepack] add dev & prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,97 @@
-module.exports = function (env, argv) {
-  return {
-    mode: "development",
-    entry: __dirname + "/src/index.js",
-    output: {
-      path: __dirname + '/dist',
-      filename: "[name].[chunkhash:8].js"
-    },
-    devtool: "source-map",
-    devServer: {
-      contentBase: __dirname + '/dist',
-      compress: true,
-      port: 9000
-    },
-    watch: true,
-    module: {
-      rules: [{
-          enforce: "pre",
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: "eslint-loader"
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const development = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/build',
+    filename: '[name].[chunkhash:8].js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './build',
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [{
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            'presets': [
+              'env',
+            ],
+          },
         },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              "presets": [
-                "env"
-              ]
-            }
-          }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
+
+const production = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/build',
+    filename: '[name].[chunkhash:8].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [{
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            'presets': [
+              'env',
+            ],
+          },
         },
-        {
-          test: /\.scss$/,
-          use: [
-            "style-loader",
-            "css-loader",
-            "sass-loader"
-          ]
-        }
-      ]
-    }
-  }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+}
+
+module.exports = function(env, argv) {
+  return env.production ? production : development
 }
