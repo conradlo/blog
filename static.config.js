331686ee6632af4f8dad4f4cafd93c22994853dc
Conrad Lo <email@conradlo.com>
2018-11-07 23:43:02 +0800

    add css loader

--- static.config.js
+++ static.config.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 export default {
   getSiteData: () => ({
@@ -34,5 +35,56 @@ export default {
         component: 'src/containers/404',
       },
     ]
+  },
+  webpack: (config, { defaultLoaders, stage }) => {
+    let loaders = []
+
+    if (stage === 'dev') {
+      loaders = [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+    } else {
+      loaders = [
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            minimize: stage === 'prod',
+            sourceMap: false,
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: { includePaths: ['src/'] },
+        },
+      ]
+
+      // Don't extract css to file during node build process
+      if (stage !== 'node') {
+        loaders = ExtractTextPlugin.extract({
+          fallback: {
+            loader: 'style-loader',
+            options: {
+              sourceMap: false,
+              hmr: false,
+            },
+          },
+          use: loaders,
+        })
+      }
+    }
+
+    config.module.rules = [
+      {
+        oneOf: [
+          {
+            test: /\.s(a|c)ss$/,
+            use: loaders,
+          },
+          defaultLoaders.cssLoader,
+          defaultLoaders.jsLoader,
+          defaultLoaders.fileLoader,
+        ],
+      },
+    ]
+    return config
   },
 }
