567eca61b0a315fd662829ae4346be9c0033f642
Conrad Lo <email@conradlo.com>
2018-11-07 23:43:35 +0800

    add normalize css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Link } from 'react-static'
+import { Router } from 'react-static'
 import { hot } from 'react-hot-loader'
 //
 import Routes from 'react-static-routes'
@@ -8,15 +8,8 @@ import './app.css'
 
 const App = () => (
   <Router>
-    <div>
-      <nav>
-        <Link exact to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-      </nav>
-      <div className="content">
-        <Routes />
-      </div>
+    <div className="content">
+      <Routes />
     </div>
   </Router>
 )
